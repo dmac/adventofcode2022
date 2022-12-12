a1d041604b6689ff080769a8ca169875b8ed7321
Daniel MacDougall <dmacdougall@gmail.com>
2022-12-11 22:41:52 -0800

    WIP: Simplify Day 11

--- 11.go
+++ 11.go
@@ -55,13 +55,17 @@ func parseMonkeys(lines []string) []*monkey {
 		case strings.HasPrefix(l, "Operation:"):
 			_, eq, _ := strings.Cut(l, "=")
 			parts := strings.Split(strings.TrimSpace(eq), " ")
-			v0 := m.getOpVal(parts[0])
-			v1 := m.getOpVal(parts[2])
 			switch parts[1] {
 			case "*":
-				m.update = makeUpdateFunc(v0, v1, &m.testDivisible, mul)
+				if parts[2] == "old" {
+					m.update = func(a int) int { return a * a }
+				} else {
+					v := mustParseInt(parts[2])
+					m.update = func(a int) int { return a * v }
+				}
 			case "+":
-				m.update = makeUpdateFunc(v0, v1, &m.testDivisible, add)
+				v := mustParseInt(parts[2])
+				m.update = func(a int) int { return a + v }
 			default:
 				panic(fmt.Sprintf("unknown op %q", parts[1]))
 			}
@@ -80,29 +84,19 @@ func parseMonkeys(lines []string) []*monkey {
 	return monkeys
 }
 
-func makeUpdateFunc(v0, v1, testDivisible *int, fn func(a, b int, c *int) int) func(item int) int {
-	return func(old int) int {
-		v0 := v0
-		v1 := v1
-		if v0 == nil {
-			v0 = &old
-		}
-		if v1 == nil {
-			v1 = &old
-		}
-		v := fn(*v0, *v1, testDivisible)
-		return v
-	}
-}
-
 func (m *monkey) inspectItems(monkeys []*monkey) {
+	// if m == monkeys[0] {
+	// 	fmt.Println(m.items)
+	// }
 	for len(m.items) > 0 {
 		m.count++
-		item := m.update(m.items[0]) // % m.testDivisible
-		if item < m.items[0] {
-			panic(fmt.Sprintf("overflow: %d, %d", item, m.items[0]))
-		}
-		m.items = m.items[1:]
+		item := m.update(m.items[0]) % m.testDivisible
+		// if m == monkeys[0] {
+		// 	fmt.Println(m.items[0], item, m.testDivisible, item%m.testDivisible == 0)
+		// }
+		// if item < m.items[0] {
+		// 	panic(fmt.Sprintf("overflow: %d, %d", item, m.items[0]))
+		// }
 		var next *monkey
 		if item%m.testDivisible == 0 {
 			next = monkeys[m.testTrue]
@@ -110,21 +104,6 @@ func (m *monkey) inspectItems(monkeys []*monkey) {
 			next = monkeys[m.testFalse]
 		}
 		next.items = append(next.items, item)
+		m.items = m.items[1:]
 	}
-}
-
-func (m *monkey) getOpVal(s string) *int {
-	if s == "old" {
-		return nil
-	}
-	n := mustParseInt(s)
-	return &n
-}
-
-func mul(a, b int, testDivisible *int) int {
-	return (a * b) // % (*testDivisible)
-}
-
-func add(a, b int, testDivisible *int) int {
-	return (a + b) // % (*testDivisible)
 }
